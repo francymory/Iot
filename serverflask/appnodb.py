3efc98deb412f0caa1a9beee91f05ec3a2fed1f3
virgi <82643700+melottiv@users.noreply.github.com>
2024-02-12 13:25:14 +0100

    Add files via upload

--- serverflask/appnodb.py
+++ serverflask/appnodb.py
@@ -1,1 +1,21 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/', methods=['POST'])
+def ricevi_richiesta():
+    dati_richiesta = request.get_json()
+    latitude=dati_richiesta.get('latitude','')
+    longitude=dati_richiesta.get('longitude','')
+    caduta=dati_richiesta.get("caduta")
+    ip=request.remote_addr
+
+    dati = {"vicini": 0, "allarme": caduta}
+    print(f"ho ricevuto da {ip}\n{latitude}\t{longitude}\t{caduta}")
+    return dati, 200
+
+if __name__ == '__main__':
+
+    app.run(host='192.168.1.74') 
